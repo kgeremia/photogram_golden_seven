e6ac6d5feea2a1e87e2fa5243d426a240414706d
kgeremia <kgermeia2016@kellogg.northwestern.edu>
2015-05-10 19:47:41 -0500

    update create and destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,8 +4,41 @@ class PhotosController < ApplicationController
   end
 
   def show
-    @list_of_photos = Photo.all
     @photo = Photo.find_by({ :id => params["id"]})
+  end
+
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def destroy
+    p = Photo.find_by({ :id => params["id"]})
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params["id"]})
+
+  end
+
+  def update_row
+    p = Photo.find(params[:id])
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos/#{p.id}")
   end
 
 end
